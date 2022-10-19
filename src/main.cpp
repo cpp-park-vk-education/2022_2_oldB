ce0addc6f5f31a5d210a5de916933a5af5bdbc56
Constantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2022-10-19 22:08:58 +0300

    Move stub

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest.h"
+
+#include <iostream>
+
+int add_one(int x);
+int program();
+
+int main(int argc, char** argv) {
+    doctest::Context context;
+    context.applyCommandLine(argc, argv);
+
+    int res = context.run();
+
+    if (context.shouldExit()) {
+        return res;
+    }
+    int prg = program();
+    return res + prg;
+}
+
+int program() {
+    std::cout << add_one(2) << std::endl;
+    return 0;
+}
+
+int add_one(int x) {
+    return x + 1;
+}
+
+TEST_CASE("add-one") {
+    CHECK(add_one(3) == 4);
+}
