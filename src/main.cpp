3eb9fbba7cc454902b487bbde0fcf1c881c7890a
Banzay <bazaleevf@gmail.com>
2022-10-30 01:16:05 +0300

    cgange main struct

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,10 @@
-#define DOCTEST_CONFIG_IMPLEMENT
 #include <iostream>
 
-#include "doctest.h"
 #include "add_one.h"
 
 int add_one(int x);
-int program();
 
-int main(int argc, char** argv) {
-    doctest::Context context;
-    context.applyCommandLine(argc, argv);
-
-    int res = context.run();
-
-    if (context.shouldExit()) {
-        return res;
-    }
-    int prg = program();
-    return res + prg;
-}
-
-int program() {
+int main() {
     std::cout << add_one(2) << std::endl;
     return 0;
-}
-
-TEST_CASE("add-one") {
-    CHECK(add_one(3) == 4);
 }
