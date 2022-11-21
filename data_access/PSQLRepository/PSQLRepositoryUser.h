a8e32595325a3a019efaee1c5ae92a95da59a99d
Fedor <bazaleevf@gmail.com>
2022-11-21 16:11:31 +0000

    add PSQLRep

---
+++ data_access/PSQLRepository/PSQLRepositoryUser.h
@@ -0,0 +1,32 @@
+class PSQLRepositoryUser
+{
+    Users GetUserByLogin(string Login) {
+    }
+
+    void InsertUser(Users u) {
+    }
+
+    void InsertRole(Role r) {
+    }
+
+    int GetNewId() {
+    }
+
+    bool CheckUserLogin(string Login) {
+    }
+
+    std::vector<Users> GetAllUsers() {
+    }
+
+    void UpdateUserInf(int Id) {
+    }
+
+    void UpdateUserRole(Users user, Role r) {
+    }
+
+    void DeleteUser(Users u) {
+    }
+
+    void DeleteUser(int Id) {
+    }
+}
