fd4b40031fb4c6cbb364d97c8fcdedf6d36518c9
Andrey Tonkoshtan <84812632+TonL@users.noreply.github.com>
2022-12-24 14:21:13 +0300

    pre release version

--- Client/Client/main.cpp
+++ Client/Client/main.cpp
@@ -9,8 +9,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
 
-    a.setWindowIcon(QIcon(":/img/img/speechbubble.png"));
-    w.setWindowTitle("Text Messenger");
+//    a.setWindowIcon(QIcon(":/img/img/speechbubble.png"));
+    w.setWindowTitle("");
 
     w.show();
     return a.exec();
