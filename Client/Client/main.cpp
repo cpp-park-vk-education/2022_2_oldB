8f7001bd46416e3dc89df596fb5e4ce8f022c788
Andrey Tonkoshtan <84812632+TonL@users.noreply.github.com>
2022-12-26 02:03:16 +0300

    before fixes

--- Client/Client/main.cpp
+++ Client/Client/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     MainWindow w;
 
 //    a.setWindowIcon(QIcon(":/img/img/speechbubble.png"));
-    w.setWindowTitle("");
+    w.setWindowTitle(" ");
 
     w.show();
     return a.exec();
